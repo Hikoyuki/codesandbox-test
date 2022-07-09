47ee0a3d8fb9ba9d7fd988bf4166dcdc0d51f4d6
Hikoyuki <63903730+Hikoyuki@users.noreply.github.com>
2022-07-09 14:05:23 +0900

    q

--- src/index.js
+++ src/index.js
@@ -68,9 +68,67 @@
 /**
  * mapやfilterを使った配列の処理
  */
-const nameArr = ["田中", "山田", "ジャケ"];
-for (let index = 0, index < nameArr.length; index++) {
-  console.log(nameArr[index]);
-}
+// const nameArr = ["田中", "山田", "ジャケ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(nameArr[index]);
+// }
 
-const name
+// 新しい配列を作る
+// const nameArr2 = nameArr.map((name)=>{
+//   return name;
+// })
+// console.log(nameArr2);
+
+// 配列の中身をそのまま出力 以前はfor文でやっていた使い方
+// nameArr.map((name) => console.log(name))
+
+// ある条件に一致したものだけ返却して新しい配列を生成する関数
+// const numArr = [1, 2, 3, 4, 5];
+// const newNumArr = numArr.filter((num) => {
+//   return num % 2 === 1;
+// });
+// console.log(newNumArr);
+
+// 配列の何番目の要素か
+// const nameArr = ["田中", "山田", "ジャケ"];
+// for (let index = 0; index < nameArr.length; index++) {
+//   console.log(`${index + 1}番目は${nameArr[index]}`);
+// }
+// mapの場合 2つ目の引数に順番に0から入る
+// nameArr.map((name, index) => console.log(`${index + 1}番目は${name}です`))
+
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "ジャケ") {
+//     return name
+//   } else {
+//     return `${name}さん`
+//   }
+// })
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// const num = "1300";
+// console.log(num.toLocaleString());
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力してください'
+// console.log(formattedNum);
+
+// return部分で使う例
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています！' : '共用範囲内です'
+// }
+// console.log(checkSum(50, 70));
+
+/**
+ * 論理演算子の方等の意味
+ */
+// ||は左側がfalseなら右側を返す　num=100とすればnumの値帰る nullはfalse判定
+const num = null;
+const fee = num || "金額未設定です";
+console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+const num2 = null;
+const fee2 = num2 && "何か設定されました";
+console.log(fee2);
